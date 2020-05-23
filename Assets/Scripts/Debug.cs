c28715194f8b1662101e167340e27f4b7161c495
Toxicat <marc.guilmard@gmail.com>
2020-05-23 16:01:46 +0200

    Base B

---
+++ Assets/Scripts/Debug.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Reflection;
+
+namespace Assets.Scripts
+{
+	public static class Debug
+	{
+		public static string ModName => Assembly.GetExecutingAssembly().GetName().Name;
+		public static string DT => DateTime.Now.ToString("G");
+
+		public static void Log(object msg)
+		{
+			UnityEngine.Debug.Log($"[{DT}]" + $"[{ModName}] : " + msg);
+		}
+
+		public static void LogWarning(object msg)
+		{
+			UnityEngine.Debug.LogWarning($"[{DT}]" + $"[{ModName}] : " + msg);
+		}
+
+		public static void LogError(object msg)
+		{
+			UnityEngine.Debug.LogError($"[{DT}]" + $"[{ModName}] : " + msg);
+		}
+	}
+}
